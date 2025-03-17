877f4167cc3545d1f70100ecf96b90caa4f2a3cd
MahmoudSayed216 <mahmoud_das@outlook.com>
2025-03-17 17:19:58 +0200

    minor changes

--- model_size_calculator.py
+++ model_size_calculator.py
@@ -1,31 +1,39 @@
 # this method is a very accurate but not an exact calculation
 from model import decoder, encoder, merger, refiner
-d = decoder.Decoder()
-e = encoder.Encoder("cpu", False)
-m = merger.Merger(0.2)
-r = refiner.Refiner(0.2, False)
+import yaml
+
+configs = None
+with open("config.yaml", "r") as f:
+    configs = yaml.safe_load(f)
+
+model_cfg = configs["model"]
+print(model_cfg)
+Encoder = encoder.Encoder(configs=model_cfg).to(configs["device"])
+Decoder = decoder.Decoder().to(configs["device"])
+Merger = merger.Merger(model_cfg["lrelu_factor"]).to(configs["device"])
+Refiner = refiner.Refiner(model_cfg["lrelu_factor"], model_cfg["use_bias"]).to(configs["device"])
 
 sum_params = lambda M: sum(p.numel() for p in M.parameters()) 
 
-parameters = sum([sum_params(M) for M in [e, d, m, r]])
+parameters = sum([sum_params(M) for M in [Encoder, Decoder, Merger, Refiner]])
 
 
 size_in_mb = parameters>>18 
 
 
-print(size_in_mb)
+print("Model size: ", size_in_mb)
 
 
 
 
-## INFO TO BE ADDED TO THE PRESENTATION
-import torchinfo
-import torch
-from Model import ALittleBitOfThisAndALittleBitOfThatNet
+# ## INFO TO BE ADDED TO THE PRESENTATION
+# import torchinfo
+# import torch
+# from Model import ALittleBitOfThisAndALittleBitOfThatNet
 
 
 
-model = ALittleBitOfThisAndALittleBitOfThatNet("cpu", 0.2, False)
+# model = ALittleBitOfThisAndALittleBitOfThatNet("cpu", 0.2, False)
 
-torchinfo.summary(model, input_data=(torch.randn(1, 5, 3, 224, 224), torch.randn(1, 5, 3, 224, 224)))
+# torchinfo.summary(model, input_data=(torch.randn(1, 5, 3, 224, 224), torch.randn(1, 5, 3, 224, 224)))
 
